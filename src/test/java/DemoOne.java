9d3ec8c864b4760d88664294c889ac795af9e1c1
Gopi Srinivasan <igopisrinivasan@gmail.com>
2021-02-16 14:11:21 +0530

    basic test case

---
+++ src/test/java/DemoOne.java
@@ -0,0 +1,20 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class DemoOne {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get("http://omayo.blogspot.com/");
+        driver.findElement(By.name("q")).sendKeys("QAFox.com");;
+        driver.findElement(By.cssSelector("input[value='Search']")).click();
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}

db96df4b82c5150da721e64540ecdf36cb45c2fa
Gopi Srinivasan <igopisrinivasan@gmail.com>
2021-02-16 14:16:34 +0530

    Testng added

--- src/test/java/DemoOne.java
+++ src/test/java/DemoOne.java
@@ -2,11 +2,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class DemoOne {
-    public static void main(String[] args) throws InterruptedException {
+
+    @Test
+    public void test() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
